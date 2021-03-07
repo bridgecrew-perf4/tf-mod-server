e9ad82b99073e4ecfcad6c49a03f23728442c563
Nicola Marco <nicola@decandia.eu>
2021-03-07 15:32:39 +0100

    colino

--- main.tf
+++ main.tf
@@ -6,9 +6,16 @@ resource "aws_s3_bucket" "testbucket" {
   bucket_prefix = "testbucket"
 }
 
+resource "aws_s3_bucket" "testbucket1" {
+  bucket_prefix = "testbucket1"
+}
+
 output "my_bucket_name" {
   value = aws_s3_bucket.testbucket.id
 }
+
+
+
 
 terraform {
   backend "remote" {
