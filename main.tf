28774d5fc80544c10a49948058d3787e1a16d346
Nicola Marco <nicola@decandia.eu>
2021-03-07 15:29:33 +0100

    nicola

--- main.tf
+++ main.tf
@@ -1,43 +1,21 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical's Owner ID on AWS
-
-  filter {
-    name   = "name"
-    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-amd64-minimal-????????"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+resource "aws_s3_bucket" "testbucket" {
+  bucket_prefix = "testbucket"
 }
 
-resource "aws_network_interface" "primary" {
-  subnet_id = var.subnet_id
+output "my_bucket_name" {
+  value = aws_s3_bucket.testbucket.id
 }
 
-resource "aws_instance" "server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  tags          = var.server_tags
+terraform {
+  backend "remote" {
+    organization = "desotech"
 
-  root_block_device {
-    encrypted   = var.encrypt_root
-    volume_size = var.root_volume_size
-  }
-
-  network_interface {
-    network_interface_id = aws_network_interface.primary.id
-    device_index         = 0
+    workspaces {
+      name = "test"
+    }
   }
 }
