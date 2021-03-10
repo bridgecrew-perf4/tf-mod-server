78717549de64315223b599bf2e9c4cf86e7bfec0
Gianluca Recchia <g.recchia@desotech.it>
2021-03-10 12:36:28 +0100

    Oops

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket" "testbucket" {
   bucket_prefix = "testbucket"
 }
 
+resource "aws_s4_bucket" "non_existing_resource" {
+  bucket_prefix = "wrong-"
+}
+
 output "my_bucket_name" {
   value = aws_s3_bucket.testbucket.id
 }
